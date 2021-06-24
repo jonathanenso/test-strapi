f7fa3ec4f7a9b459e6b8e91ed081d90c1ecec6cb
Jonathan Sosa <jonathansosadev@gmail.com>
2021-06-24 12:27:55 -0400

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1565f38d0c79d0eb15166ee249796a10'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ced99627-b4d6-4964-9539-69a48fb91fba'
+};
